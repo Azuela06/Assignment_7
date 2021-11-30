26adac9919870af355093555e5d2bc0de9f28b27
Azii <violetlovato05@gmail.com>
2021-11-30 21:19:08 +0800

    Finally done, with a lot of testing of course

--- Passcode.py
+++ Passcode.py
@@ -1,2 +1,26 @@
 import re
 
+print("Please input a passcode which contains atleast a capital letter, a number, a special character [!@#_$%^&*+-/\><], and greater than 15 characters")
+
+
+def PassValidate():
+    passcode = input("Please put a strong password: ")
+    valid = True
+    for valid in passcode:
+        if (len(passcode) < 15):
+            print("Invalid passcode")
+            break
+        elif not re.search('[A-Z]', passcode):
+            print("Invalid passcode")
+            break 
+        elif not re.search('[0-9]', passcode):
+            print("Invalid passcode")
+            break
+        elif not re.search('[!@#_$%^&*+-/]', passcode):
+            print("Invalid pascode")
+            break
+        else:
+            print("Passcode inputted is valid")
+            break
+
+PassValidate()
